ccc86eb617fc6d3463ef1dcf8286e5bd4a439388
Mohd Mushfique <mushfique@digitalagents.in>
2019-10-09 15:52:39 +0530

    added navigation and redux files

---
+++ src/screens/splashscreen/Splashscreen.styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  heading: {
+    color: '#000',
+    fontSize: 20
+  }
+})
