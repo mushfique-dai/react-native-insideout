1b84619d3e86d2314e540828dba8f5e129f52583
Mushfique <mushfique6@gmail.com>
2022-10-31 19:45:11 +0530

    fixed common state and added Main tabs screen

---
+++ src/screens/profile.js
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+function Profile() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Profile</Text>
+    </View>
+  );
+}
+
+export default Profile;
